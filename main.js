794bb3e5d33ef97b069f60fd24a03ada2415942f
Bhoomi Joshi <bhoomijoshi31@gmail.com>
2022-01-18 19:13:58 +0530

    v1.0.18

--- main.js
+++ main.js
@@ -44,13 +44,13 @@ ipcMain.on('app_version', (event) => {
   event.sender.send('app_version', { version: app.getVersion() });
 });
 
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
-});
+// autoUpdater.on('update-available', () => {
+//   mainWindow.webContents.send('update_available');
+// });
 
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
-});
+// autoUpdater.on('update-downloaded', () => {
+//   mainWindow.webContents.send('update_downloaded');
+// });
 
 ipcMain.on('restart_app', () => {
   autoUpdater.quitAndInstall();
